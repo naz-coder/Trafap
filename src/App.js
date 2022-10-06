2bc254bad93add696615164d06a1c4044484ad60
naz-coder <favouritenaz@yahoo.com>
2022-10-06 03:07:34 +0100

    Landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-import Nav_Home from "./components/Nav_Home/nav";
 import { BrowserRouter as Route, Routes, Router } from "react-router-dom";
+import Nav_Home from "./components/Nav_Home/nav";
+import Home from "./pages/Home/home";
 
 function App() {
   return (
     <div className="container">
       <Nav_Home />
+      <Home />
       <h1>Testing</h1>
     </div>
   );
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,8 +1,18 @@
 import React from "react";
+import "./home.css";
+import landing from "../../components/assets/images/landing.png";
 
 const home = () => {
   return (
-    <div>
+    <div className="home_flex">
+      <div className="flex_left">
+        <h3>What is Your Choice Location? Let's Help You Beat Traffic!</h3>
+        <p>Search traffic by route...</p>
+      </div>
+      <div className="flex_right">
+        <input placeholder="Enter Route Here..."></input>
+        <button className="home_btn">Search</button>
+      </div>
     </div>
   );
 };
