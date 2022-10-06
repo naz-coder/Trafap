44613f2eb22c949b919fac29e73a09b927efefa1
naz-coder <favouritenaz@yahoo.com>
2022-10-06 14:49:30 +0100

    Hero Session

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -4,14 +4,22 @@ import landing from "../../components/assets/images/landing.png";
 
 const home = () => {
   return (
-    <div className="home_flex">
+    <div className="landing">
+    <div className="hero">
       <div className="flex_left">
-        <h3>What is Your Choice Location? Let's Help You Beat Traffic!</h3>
+        <h3>
+          What is Your Choice Location? Let's Help You Beat Traffic and Getting
+          You to Your Destination in No Time!
+        </h3>
         <p>Search traffic by route...</p>
       </div>
       <div className="flex_right">
         <input placeholder="Enter Route Here..."></input>
         <button className="home_btn">Search</button>
+      </div>
+      </div>
+      <div className="slide">
+        <h3>Latest Traffic Updates</h3>
       </div>
     </div>
   );
